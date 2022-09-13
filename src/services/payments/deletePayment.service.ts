d1fdbaf789667f2bf1213c6b2c428e9dc5c06b0a
anjosdelacerda <anjosdelacerda@gmail.com>
2022-09-13 18:17:16 -0300

    update: limpando o código da paymentDelete

--- src/services/payments/deletePayment.service.ts
+++ src/services/payments/deletePayment.service.ts
@@ -8,23 +8,17 @@ const deletePaymentService = async (id: string) => {
     throw new AppError("access denied", 404);
   }
 
-  console.log(id, "id recebido do controller");
-
   const profileRepository = AppDataSource.getRepository(Profile);
 
   const profiles = await profileRepository.find();
 
   const profile = profiles.find((prof) => prof.id === id);
 
-  console.log(profile, "2: profile encontrado a partir do id");
-
   if (!profile) {
     throw new AppError("user not found", 404);
   }
 
   const findPayment = profile!.payment_info_id.id;
-
-  console.log(findPayment, "3: id do payment");
 
   if (!findPayment) {
     throw new AppError("user does not have a payment method");
@@ -41,8 +35,6 @@ const deletePaymentService = async (id: string) => {
   const payments = await paymentRepository.find();
 
   const payment = payments.find((pay) => pay.id === findPayment);
-
-  console.log(payment, "4: payment a ser deletado");
 
   await paymentRepository.delete(payment!.id);
 
