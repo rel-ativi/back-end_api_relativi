0393a482030ad505c5b97fd26ccfdb7967211de3
PandaSuzuki <renatosuzuki03@gmail.com>
2022-09-13 10:45:40 -0300

    fix: fixed find update profiles service and controller

--- src/services/profiles/profilesUpdate.service.ts
+++ src/services/profiles/profilesUpdate.service.ts
@@ -3,11 +3,11 @@ import { Profile } from "../../entities/profiles.entity";
 import { AppError } from "../../errors/AppError";
 import { IProfile } from "../../interfaces/profiles";
 
-const profilesUpdateService = async ({bio, phone}: IProfile) => {
+const profilesUpdateService = async (profile_id: string, {bio, phone}: IProfile) => {
     const profileRepo = AppDataSource.getRepository(Profile)
     const profiles = await profileRepo.find()
 
-    const profile = profiles.find((prof) => prof.phone === phone)
+    const profile = profiles.find((prof) => prof.id === profile_id)
 
     if(!profile) {
         throw new AppError("Profile not exists", 404)
