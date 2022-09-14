ec66381b8c6871e2a44f9d3afca72032ee7830a4
Hugo Ernesto <mail.hbler@gmail.com>
2022-09-14 11:13:15 -0300

    feat: updating dist

--- dist/interfaces/generic/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.INameNumber = exports.INameOnly = void 0;
-class INameOnly {
-}
-exports.INameOnly = INameOnly;
-class INameNumber {
-}
-exports.INameNumber = INameNumber;
